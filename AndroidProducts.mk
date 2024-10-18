adbe80734a49cc7c9737476c4b85557eeaf007c2
FelipeCH <120758314+lipef23937@users.noreply.github.com>
2024-10-18 17:27:10 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,10 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_java.mk
+add_lunch_combo := \
+   twrp_java-user \
+    twrp_java-userdebug \
+    twrp_java-eng
 
 COMMON_LUNCH_CHOICES := \
     twrp_java-user \
