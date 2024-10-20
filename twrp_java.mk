029a0984cc4c3328214f8be66996be5709f6614b
FelipeCH <120758314+lipef23937@users.noreply.github.com>
2024-10-20 02:44:27 -0300

    Update twrp_java.mk

--- twrp_java.mk
+++ twrp_java.mk
@@ -11,13 +11,9 @@ PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(20)
 PRODUCT_MANUFACTURER := motorola
 
-# Inherit from those products. Most specific first.
+# Inherit from java device
+$(call inherit-product, device/motorola/java/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-
-# Inherit from java device
-$(call inherit-product, device/motorola/java/device.mk)
